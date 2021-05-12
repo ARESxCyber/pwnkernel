8dc841a51c23e58cd6a81e84d61557781bd99af2
qxxxb <46016483+qxxxb@users.noreply.github.com>
2021-05-12 01:51:29 -0400

    Fix URL

--- helper_scripts/extract_vmlinux_wrapper.sh
+++ helper_scripts/extract_vmlinux_wrapper.sh
@@ -1,7 +1,7 @@
 # Extract vmlinux ELF from bzImage so that we can debug the kernel with GDB
 
 # Note: If you want to have debug symbols, you can compile the kernel as shown
-# here: https://github.com/pwncollege/pwnkernel/blob/main/build.sh
+# here: https://github.com/ARESxCyber/pwnkernel/blob/master/build_kernel.sh
 # Unfortunately it takes a long time to compile but is very helpful.
 
 wget -O extract-vmlinux https://raw.githubusercontent.com/torvalds/linux/master/scripts/extract-vmlinux
