b0057eae6fabb2b1cb3d89391977512dc2fa4f3c
qxxxb <46016483+qxxxb@users.noreply.github.com>
2021-05-12 01:19:41 -0400

    Update client.py

--- helper_scripts/client.py
+++ helper_scripts/client.py
@@ -2,6 +2,7 @@ import os
 import gzip
 import base64
 import time
+from pathlib import Path
 
 os.environ["PWNLIB_NOTERM"] = "true"
 import pwn
@@ -16,28 +17,29 @@ def chunks(xs, n):
         yield xs[i : i + n]
 
 
-def send_file(io, filename):
-    s = open(filename, "rb").read()
+def send_file(io, filepath):
+    s = open(filepath, "rb").read()
     s = gzip.compress(s)
     s = base64.b64encode(s).decode()
 
-    print(f"Sending {filename}")
+    print(f"Sending {filepath}")
     for i, chunk in enumerate(chunks(s, 128)):
         io.sendline(f"echo -n {chunk} | base64 -d > {i:04}.part")
         print(".", end="", flush=True)
     print()
 
-    io.sendline(f"cat * > {filename}.gz")
-    io.sendline(f"gzip -d {filename}.gz")
+    io.sendline(f"cat * > {filepath.name}.gz")
+    io.sendline(f"gzip -d {filepath.name}.gz")
     io.sendline("rm *.part")
 
 
-io = pwn.remote("35.224.45.136", 1001)
+io = pwn.remote(pwn.args["HOST"], int(pwn.args["PORT"]))
 
 pwn.context.log_level = "debug"
 io.recvuntil("$")
 pwn.context.log_level = "info"
 
-send_file(io, "solve")
-io.sendline("chmod +x solve")
+filepath = Path(pwn.args["EXPLOIT"])
+send_file(io, filepath)
+io.sendline(f"chmod +x {filepath.name}")
 io.interactive()
